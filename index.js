080a4e8892f3c7e8935cd3b1b33ea181e00e41d6
VPoshelyuk <aslava198@gmail.com>
2019-11-04 14:30:46 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let res = record.find(function(record){return record.result === "W"})
+  if(res){
+    return res.year
+  } else {
+    return undefined
+  }
+}
